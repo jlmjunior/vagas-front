5b9f3fe37f514f847d723c6d095fb6d84b9b4b97
Junior Martinez <jlmjunior@live.com>
2020-11-22 03:49:54 -0300

    Add pages: sobre e vagas

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+const sobre = () => {
+  return (
+    <div>
+      <Link href="/">
+        voltar
+      </Link>
+    </div>
+  )
+}
+
+export default sobre
---
+++ pages/vagas.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+const vagas = () => {
+  return (
+    <div>
+      <Link href="/">
+        voltar
+      </Link>
+    </div>
+  )
+}
+
+export default vagas
