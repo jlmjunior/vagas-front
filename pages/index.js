99f3f826f55d9392548c64d265f4dcb114329de3
Junior Martinez <jlmjunior@live.com>
2020-11-24 22:02:11 -0300

    Título

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Vagas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
